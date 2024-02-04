dcd3cb87cefc2d6b497e8f8c3866b7be505d0d51
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-02-04 23:58:21 +0530

    Template Engine

--- Express/Remove extension from URL and 404 Page.js
+++ Express/Remove extension from URL and 404 Page.js
@@ -1,9 +1,14 @@
- const express=require("express")
+
+const express=require("express")
  const path= require('path')
   const app = express()
 
    const publicepath=path.join(__dirname,'publice')
- 
+
+   const views=path.join(__dirname,'views')
+
+    app.set('view engine','ejs')
+
     app.get('',(req,resp)=>{
         resp.sendFile(`${publicepath}/index.html`)
     })
@@ -13,6 +18,11 @@
     app.get('/help',(req,resp)=>{
         resp.sendFile(`${publicepath}/help.html`)
     })
+    // one
+     app.get('/profile',(req,resp)=>{
+        const user={name:"Ravikumar Pulivarthi",email:"ravi@1998,gail.com",age:25,study:"MCA"}
+        resp.render(`${views}/profile`,{user})
+     })
     app.get('*',(req,resp)=>{
         resp.sendFile(`${publicepath}/error.html`)
     })
