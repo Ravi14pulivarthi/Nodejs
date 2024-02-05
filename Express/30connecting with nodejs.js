4d247e09c55656c7d7ecd5be499119b1ae4de787
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-02-05 23:38:39 +0530

    connection with  nodejs

---
+++ Express/30connecting with nodejs.js
@@ -0,0 +1,14 @@
+const {MongoClient } = require("mongobd");
+const url = "mongodb://localhost:27017";
+const batabasename = "E-commers";
+const client = new MongoClient(url);
+
+async function getData() {
+  let result = await client.connect();
+  db = result.db(batabasename);
+  collection = db.collection("Products");
+  let data = await collection.find({}).toArray();
+  console.log(data);
+}
+
+getData();
