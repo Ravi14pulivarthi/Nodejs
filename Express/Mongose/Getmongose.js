92b3fdb910871f9e40a0cac8da12e430c6fb1e25
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-02-10 20:25:53 +0530

    GetMongoes

--- Express/Mongose/Getmongose.js
+++ Express/Mongose/Getmongose.js
@@ -4,8 +4,8 @@
 
     const  app=express()
 
-
-     app.get("get/",async(req,resp)=>{
+  app.use(express.json())
+     app.get("/get",async(req,resp)=>{
         let data=await Mongosmain.find()
          resp.send(data)
      })
